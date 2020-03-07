5d86eb859a801b3c67002443253542c62de0d9b6
AlexandruLis <alexandru.lisca.iosif@gmail.com>
2020-03-07 12:41:16 +0200

    Added a proper rategirl command
    This new commands requires dependencies in classified/globals
    Basically generates 2 random values, calculates in which polygon they belong and draws a graph png.

--- src/commands/RateGirl/cog.py
+++ src/commands/RateGirl/cog.py
@@ -1,11 +1,55 @@
 from discord.ext import commands
+import discord
 from src.commands.RateGirl.rategirl import RateGirl
-import random
+from PIL import Image, ImageDraw, ImageFont
+from classified.globals import not_a_girl, spam_guild_id, spam_guild_channel as spam_channel_id
 
 
 class DiscordRateGirl(commands.Cog):
     def __init__(self, bot):
         self.client = bot
+
+    @staticmethod
+    def draw_the_picture(xpoint, ypoint):
+        img = Image.open('../assets/small_heart.png', 'r')
+        im = Image.new('RGBA', (1200, 1200), (255, 255, 255, 255))
+        font = ImageFont.truetype("../assets/ManhattanDarling-Regular.ttf", size=50)
+        draw = ImageDraw.Draw(im)
+        # draw the graph
+        draw.line(xy=[(100, 700), (1100, 100)], fill=(0, 0, 0), width=3)  # Hot-crazy border line
+        draw.line(xy=[(100, 700), (100, 100)], fill=(0, 0, 0), width=3)  # Crazy axis
+        draw.line(xy=[(600, 700), (600, 100)], fill=(0, 0, 0), width=3)  # 5 hot line
+        draw.line(xy=[(800, 1100), (800, 280)], fill=(0, 0, 0), width=3)  # 8 Hot line
+        draw.line(xy=[(800, 400), (1100, 400)], fill=(0, 0, 0), width=3)  # Wife to date zone separator
+        draw.line(xy=[(800, 600), (1100, 600)], fill=(0, 0, 0), width=3)  # Date to unicorn separator
+        draw.line(xy=[(100, 700), (1100, 700)], fill=(0, 0, 0), width=3)  # Hot Axis
+        # draw the text
+        draw.text(xy=(40, 40), text="Crazy", fill=(255, 0, 0), font=font)
+        draw.text(xy=(1120, 680), text="Hot", fill=(255, 0, 0), font=font)
+
+        draw.text(xy=(250, 300), text="NO-GO", fill=(0, 0, 0), font=font)
+
+        draw.text(xy=(700, 130), text="DANGER", fill=(0, 0, 0), font=font)
+        draw.text(xy=(700, 170), text="ZONE", fill=(0, 0, 0), font=font)
+
+        draw.text(xy=(670, 450), text="FUN", fill=(0, 0, 0), font=font)
+        draw.text(xy=(670, 490), text="ZONE", fill=(0, 0, 0), font=font)
+
+        draw.text(xy=(930, 250), text="DATE", fill=(0, 0, 0), font=font)
+        draw.text(xy=(930, 290), text="ZONE", fill=(0, 0, 0), font=font)
+
+        draw.text(xy=(930, 450), text="WIFE", fill=(0, 0, 0), font=font)
+        draw.text(xy=(930, 490), text="ZONE", fill=(0, 0, 0), font=font)
+
+        draw.text(xy=(900, 640), text="UNICORNS", fill=(0, 0, 0), font=font)
+
+        draw.text(xy=(910, 760), text=not_a_girl.upper(), fill=(0, 0, 0), font=font)
+        # get the heart
+        im.paste(img, [xpoint, ypoint], mask=img)
+        del draw
+
+        # write to stdout
+        im.save('../assets/rate_girl_result.png', "PNG")
 
     @commands.command(pass_context=True)
     async def rategirl(self, ctx):
@@ -14,13 +58,38 @@ class DiscordRateGirl(commands.Cog):
         :param ctx: Context from message
         :return: None
         """
+
         user_id = float(
             "".join([s for s in ctx.message.content.split()[1] if s.isdigit()]))  # transform user id into a float
         try:
-            hot, crazy = RateGirl().rategirl(user_id)  # Call function for pseudo rng
-            await ctx.channel.send("Hot: {} Crazy: {}".format(hot, crazy))  # Send formatted message
+            hot_value, crazy_value, hot_position, crazy_position, result = RateGirl().rategirl(
+                user_id)  # Call function for pseudo rng
+
+            print(hot_value, crazy_value, hot_position, crazy_position, result)
+            self.draw_the_picture(hot_position, crazy_position)
+
+            embed = discord.Embed()
+            embed.add_field(name='Hot', value=str(hot_value))
+            embed.add_field(name='Crazy', value=str(crazy_value))
+            embed.add_field(name='Status', value=result, inline=False)
+            link = await self.generate_picture_link()
+            embed.set_image(url=link)
+            await ctx.channel.send(embed=embed)  # Send formatted message
         except ValueError or ZeroDivisionError:
             pass
+
+    async def generate_picture_link(self):
+        spam_channel = None
+        embed_picture = None
+        for guild in self.client.guilds:
+            if guild.id == spam_guild_id:
+                for channel in guild.channels:
+                    if channel.id == spam_channel_id:
+                        spam_channel = channel
+        await spam_channel.send(file=discord.File('../assets/rate_girl_result.png'))
+        async for message in spam_channel.history(limit=1):
+            embed_picture = message.attachments[0].url
+        return embed_picture
 
 
 def setup(bot):
--- src/commands/RateGirl/rategirl.py
+++ src/commands/RateGirl/rategirl.py
@@ -1,14 +1,60 @@
 import datetime
 import time
+from classified.globals import not_a_girl
+
 
 class RateGirl:
-    def rategirl(self, user_id: float):
-        """
-        Using pseudo RNG, generate 2 numbers between 0-10 (float)
-        The RNG formula is based on day and month as well as the user ID given
-        :param user_id: float
-        :return: Hot(Float) , Crazy (Float)
-        """
+
+    def __init__(self):
+        self.zones = {
+            'No-go zone': [(100, 699), (100, 100), (599, 100), (599, 1100)],
+            not_a_girl: [(100, 700), (100, 1100), (1100, 701), (1100, 1100)],
+            'Danger zone': [(601, 399), (601, 100), (1100, 99)],
+            'Fun zone': [(600, 700), (799, 700), (799, 279), (600, 400)],
+            'Date zone': [(800, 399), (1101, 399), (1101, 100), (800, 280)],
+            'wife zone': [(800, 400), (1101, 400), (800, 599), (1101, 599)],
+            'Unicorn': [(800, 600), (1101, 600), (800, 700), (1101, 700)]
+        }
+
+    @staticmethod
+    def generate_point_coords(hot, crazy):
+        xcoord = int((hot + 1) * 100)
+        ycoord = 1200 - int((crazy + 1) * 100)
+        return xcoord, ycoord
+
+    @staticmethod
+    def point_in_poly(vcoord_x: list, vcoord_y: list, pcoord: list):  # ray casting to the right
+        inside = False
+        j = len(vcoord_x) - 1
+        for i in range(len(vcoord_x)):
+            if (((vcoord_y[i] > pcoord[1]) != (vcoord_y[j] > pcoord[1])) and
+                    (pcoord[0] < (vcoord_x[j] - vcoord_x[i]) * (pcoord[1] - vcoord_y[i]) / (vcoord_y[j] - vcoord_y[i]) +
+                     vcoord_x[i])):
+                inside = not inside
+            j = i
+
+        return inside
+
+    def determine_zone(self, point):
+        zones = self.zones
+        for zone in zones:
+            xcoord, ycoord = self.poly_format(zones[zone])
+            if self.point_in_poly(xcoord, ycoord, point):
+                return zone
+        return 'error'
+
+    @staticmethod
+    def poly_format(zone: list):
+        x_coords = []
+        y_coords = []
+        for x in zone:
+            x_coords.append(x[0])
+        for y in zone:
+            y_coords.append(y[1])
+        return x_coords, y_coords
+
+    @staticmethod
+    def generate_random_values(user_id: float):
         a = 12345
         m = 2 ** 32
         seed = int(time.mktime(datetime.date.today().timetuple()) / 100000)
@@ -22,3 +68,17 @@ class RateGirl:
         # is above?
 
         return hot, crazy
+
+    def rategirl(self, user_id: float):
+        """
+        Using pseudo RNG, generate 2 numbers between 0-10 (float)
+        The RNG formula is based on day and month as well as the user ID given
+        :param user_id: float
+        :return: Hot(Float) , Crazy (Float)
+        """
+        xpoint, ypoint = self.generate_random_values(user_id)
+        print(xpoint, ypoint)
+        xpoint_safe, ypoint_safe = self.generate_point_coords(xpoint, ypoint)
+        result = self.determine_zone([xpoint_safe, ypoint_safe])
+        print(result)
+        return xpoint, ypoint, xpoint_safe, ypoint_safe, result
